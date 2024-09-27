54aa1138a5d8f2e51dcc1b24d58691c02b5c0bd4
Evan Sizemore <evan@wool.homes>
2024-09-27 12:15:29 -0400

    hopefully successful modernization efforts for eslint

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+// @ts-ignore
+import globals from "globals";
+// @ts-ignore
+import pluginJs from "@eslint/js";
+// @ts-ignore
+import tseslint from "typescript-eslint";
+// @ts-ignore
+import react from "eslint-plugin-react";
+
+
+export default [
+
+  {
+    ignores: ["**/dist/**", "**/node_modules/**"],
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    plugins: {
+      react: react
+    },
+    settings: {
+      react: {
+        version: "detect"
+      }
+    }
+  },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  react.configs.flat.recommended,
+
+];
